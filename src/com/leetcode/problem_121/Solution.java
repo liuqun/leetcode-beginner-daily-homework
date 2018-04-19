f29e8c18266eb9406b5baeac8a3a43e288a05556
Liu Qun (liuqun) <517067180@qq.com>
2018-04-19 10:54:02 +0800

    题目121. 细节优化
    减少临时变量的数量

--- src/com/leetcode/problem_121/Solution.java
+++ src/com/leetcode/problem_121/Solution.java
@@ -5,21 +5,16 @@ public class Solution {
 	// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 	// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 	public static int maxProfit(int[] prices) {
-		int i;
 		int lowestPriceEver;
 		int profit;
-		int delta;
 
 		profit = 0;
 		lowestPriceEver = Integer.MAX_VALUE;
-		for (i = 0; i < prices.length; i++) {
-			if (prices[i] < lowestPriceEver) {
-				lowestPriceEver = prices[i];
+		for (int price : prices) {
+			if (price < lowestPriceEver) {
+				lowestPriceEver = price;
 			}
-			delta = prices[i] - lowestPriceEver;
-			if (delta > profit) {
-				profit = delta;
-			}
+			profit = Math.max(profit, price - lowestPriceEver);
 		}
 		return profit;
 	}
