abe93006960f334ed46964630c07c5ef3469de33
Liu Qun (liuqun) <517067180@qq.com>
2018-04-24 11:16:38 +0800

    补充题目217的一种解法
    - 利用Python内置的set()可以一行代码解题
    - 添加若干条单元测试用例

---
+++ src/com/leetcode/problem_217/leetcode-217.py
@@ -0,0 +1,46 @@
+﻿# -*- encoding:utf-8 -*-
+class Solution:
+    """The solution for LeetCode No.217: Contains Duplicate
+
+    https://leetcode.com/problems/contains-duplicate/
+    https://leetcode-cn.com/problems/contains-duplicate/
+    """
+
+    def containsDuplicate(self, nums):
+        """(Wrapper method for LeetCode online judge only)
+
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return contains_duplicate(nums)
+
+
+def contains_duplicate(nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    return len(nums) > len(set(nums))
+
+
+from unittest import TestCase
+
+
+class SolutionTest(TestCase):
+    def test_contains_duplicate(self):
+        assert not contains_duplicate([])
+        assert not contains_duplicate([1])
+        assert not contains_duplicate([1, 2])
+        assert contains_duplicate([1, 1])
+        assert contains_duplicate([1, 1, 1])
+        assert contains_duplicate([1, 1, 2])
+        assert contains_duplicate([1, 2, 1])
+        assert contains_duplicate([1, 2, 2])
+        assert not contains_duplicate([1, 2, 3])
+
+
+if '__main__' == __name__:
+    from os.path import abspath
+    import pytest
+
+    pytest.main(['-q', abspath(__file__)])
